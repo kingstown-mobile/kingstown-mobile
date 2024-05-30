b526b5e0fd86b76add7a20d19ad993cbedca0927
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-30 16:20:29 -0300

    botao-turmas

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign, SimpleLineIcons, Octicons, Ionicons } from '@expo/vector-icons';
 
-import CursoScreen from '../curso';
 import ConfiguracoesScreen from '../configuracoes'
 import LinksScreen from '../links';
 import TurmasScreen from '../turmas'
@@ -12,7 +11,7 @@ export default function Home() {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen name="Configurações" component={ConfiguracoesScreen} options={{ tabBarIcon: () => <Ionicons name="settings-outline" size={24} color="black" /> }} />
-            <Tab.Screen name="Home" component={TurmasScreen} options={{ tabBarIcon: () => <SimpleLineIcons name="home" size={24} color="black" /> }} />
+            <Tab.Screen name="Minhas Turmas" component={TurmasScreen} options={{ tabBarIcon: () => <AntDesign name="book" size={24} color="black" /> }} />
             <Tab.Screen name="Perfil" component={LinksScreen} options={{ tabBarIcon: () => <SimpleLineIcons name="user" size={24} color="black" /> }} />
         </Tab.Navigator>
     )
