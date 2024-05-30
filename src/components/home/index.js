005f389aae055c8ffbf0d5e105bfc360615f1355
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-30 16:06:45 -0300

    home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,16 +2,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign, SimpleLineIcons, Octicons, Ionicons } from '@expo/vector-icons';
 
 import CursoScreen from '../curso';
-import SolicitacoesScreen from '../solicitacoes';
+import ConfiguracoesScreen from '../configuracoes'
 import LinksScreen from '../links';
+import TurmasScreen from '../turmas'
 
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
     return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Configurações" component={CursoScreen} options={{ tabBarIcon: () => <Ionicons name="settings-outline" size={24} color="black" /> }} />
-            <Tab.Screen name="Home" component={SolicitacoesScreen} options={{ tabBarIcon: () => <SimpleLineIcons name="home" size={24} color="black" /> }} />
+            <Tab.Screen name="Configurações" component={ConfiguracoesScreen} options={{ tabBarIcon: () => <Ionicons name="settings-outline" size={24} color="black" /> }} />
+            <Tab.Screen name="Home" component={TurmasScreen} options={{ tabBarIcon: () => <SimpleLineIcons name="home" size={24} color="black" /> }} />
             <Tab.Screen name="Perfil" component={LinksScreen} options={{ tabBarIcon: () => <SimpleLineIcons name="user" size={24} color="black" /> }} />
         </Tab.Navigator>
     )
