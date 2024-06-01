ee18694617da9aa42751e0220177deaaccff209c
JoseHenriiquep <josehenriquenetinho@gmail.com>
2024-05-31 21:21:25 -0300

    .

--- src/components/horizontalLine/index.js
+++ src/components/horizontalLine/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-export default function Line(){
+function Line(){
     <View style={styles.container}>  
     </View>    
 }
 
 const styles = StyleSheet.create({
     container:{
-        height: 1,
-        width: 100,
-        backgroundColor: 'black',
+        borderWidth: 2,
+        width: '100%',
+        backgroundColor: '#000',
     }
 });
+
+export default Line;
